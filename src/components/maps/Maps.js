f9619826243613404feae19e10b1160463e61c5d
h4x0rlol <mazafaker.com@gmail.com>
2019-11-18 18:59:38 +0600

    added map with markers

--- src/components/maps/Maps.js
+++ src/components/maps/Maps.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react'
-import Jumbotron from 'react-bootstrap/Jumbotron'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 
-
-export default class Maps extends Component {
-    render() {
-        return (
-            <div>
-                <Jumbotron>
-                    <h1>Google maps now is not free</h1>
-                    <p>
-                        ¯\_(ツ)_/¯
-                    </p>
-                </Jumbotron>
-            </div>
-        )
+const style = {
+    map: {
+      height: '400px',
+      width: '100%'
     }
-}
+  }
+
+export default class Maps extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        markers: []
+      };
+    }
+    
+    addMarker = (e) => {
+        const {markers} = this.state
+        markers.splice(0, 2,e.latlng)
+        this.setState({markers})
+        console.log(markers)
+      }
+  
+    render() {
+      return (
+        <Map 
+          center={[19.4100819, -99.1630388]} 
+          onClick={this.addMarker}
+          zoom={13} 
+          style={style.map}
+          >
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+          />
+          {this.state.markers.map((position, idx) => 
+            <Marker key={`marker-${idx}`} position={position}>
+            <Popup>
+              <span>Popup</span>
+            </Popup>
+          </Marker>
+          )}
+        </Map>
+      );
+    }
+  }
+
